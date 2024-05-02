4b7758e4ef6d8fcdc15a06b1863fc0df39d1adb3
Gilson <gilson.dy@gmail.com>
2024-05-01 23:59:25 -0300

    feat: Products tags

--- src/app/views/products/products.module.ts
+++ src/app/views/products/products.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
@@ -20,6 +20,9 @@ import { FormsModule } from '@angular/forms';
     ProductsRoutingModule,
     FormsModule,
     SharedModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ]
 })
 export class ProductsModule { }
